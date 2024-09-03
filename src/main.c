dde51d3141ebf50d901dda66a3a93e76befb8746
Villy-P <valeriuspetrini@gmail.com>
2024-09-03 15:49:05 -0500

    feat: add error for no compiler argument

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+        if (strcmp(argv[i], "-f")) {
+            printf("Found compiler argument -f");
+            if (i == argc - 2) {
+                printf("\033[31mDid not find file name for compiler argument -f\033[0m\n");
+                exit(1);
+            }
+        }
     }
 }
