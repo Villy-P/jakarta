e394915f7ce0ab21dbdc45b94b0111de14f38a07
Villy-P <valeriuspetrini@gmail.com>
2024-09-05 15:11:28 -0500

    feat: populate enum

--- include/parser.h
+++ include/parser.h
@@ -1,4 +1,15 @@
 #ifndef __JAKARTA_ERROR_H__
 #define __JAKARTA_ERROR_H__
 
+typedef enum {
+    indentation,
+    comma,
+    period,
+    quotation,
+    apostrophe,
+    function,
+    open_parenthesis,
+    close_parenthesis
+} Symbol;
+
 #endif
