fd43ccc86bc988da8d4ffd358f227c383f471f35
Villy-P <valeriuspetrini@gmail.com>
2024-09-05 15:53:49 -0500

    chore: add STRING_BUFFER_LENGTH definition

--- src/cmd.c
+++ src/cmd.c
@@ -5,13 +5,15 @@
 #include "error.h"
 #include "parser.h"
 
+#define STRING_BUFFER_LENGTH 256
+
 void jakarta_cmd_read_file(int i, int argc, char* file_location) {
     if (i == argc - 1)
         jakarta_error_no_file_location("-f");
     FILE* file_ptr = fopen(file_location, "r");
     if (file_ptr == NULL)
         jakarta_error_file_does_not_exist(file_location);
-    char buffer[256];
+    char buffer[STRING_BUFFER_LENGTH];
     while (fgets(buffer, sizeof(buffer), file_ptr)) {
         read_line(buffer);
     }
