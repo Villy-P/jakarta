686c5cb07a167d292a0e9c4907d60f9955effcf1
Villy-P <valeriuspetrini@gmail.com>
2024-09-05 15:52:02 -0500

    feat: read in strings

--- src/parser.c
+++ src/parser.c
@@ -1,17 +1,35 @@
 #include <regex.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "parser.h"
 
+#define MAX_REGEX_GROUPS 1
+#define REGEX_FLAGS 0
+
 void read_line(char* line) {
     regex_t reegex;
+    regmatch_t groups[MAX_REGEX_GROUPS];
     int value;
-    value = regcomp(&reegex, "^\\w+", REG_EXTENDED);
-    value = regexec(&reegex, line, 0, NULL, 0);
+    value = regcomp(&reegex, "^(\\w+)", REG_EXTENDED);
+    value = regexec(&reegex, line, MAX_REGEX_GROUPS, groups, REGEX_FLAGS);
 
     if (value == REG_OK) {
-        printf("%s", line);
+        printf("%s\n", line);
+        for (unsigned int g = 0; g < MAX_REGEX_GROUPS; g++){
+            if (groups[g].rm_so == (size_t)-1)
+                break;
+            char sourceCopy[strlen(line) + 1];
+            strcpy(sourceCopy, line);
+            sourceCopy[groups[g].rm_eo] = 0;
+            char* str = sourceCopy + groups[g].rm_so;
+            printf("Group %u: [%2u-%2u]: %s\n",
+                    g, groups[g].rm_so, groups[g].rm_eo,
+                    str);
+        }
     } else if (value == REG_NOMATCH) {
         printf("No line detected");
     }
+
+    regfree(&reegex);
 }
