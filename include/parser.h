33a42d109dae5a887e96c043b4216b8d0510aa34
Villy-P <valeriuspetrini@gmail.com>
2024-09-09 15:46:42 -0500

    feat: readlines

--- include/parser.h
+++ include/parser.h
@@ -24,6 +24,6 @@ extern unsigned int token_length;
 
 void read_line(char* line);
 
-char* get_string(char* line);
+char* get_string(char** line);
 
 #endif
--- src/parser.c
+++ src/parser.c
@@ -7,6 +7,7 @@
 
 #define INITIAL_TOKENS_LENGTH 64
 #define MAX_REGEX_GROUPS 1
+#define STRING_UNEQUAL 1
 #define REGEX_FLAGS 0
 
 Token** tokens = NULL;
@@ -15,32 +16,46 @@ unsigned int token_length = INITIAL_TOKENS_LENGTH;
 void read_line(char* line) {
     if (tokens == NULL)
         tokens = malloc(sizeof(Token) * INITIAL_TOKENS_LENGTH);
-    printf("1: %s\n", get_string(line));
+    printf("Line: %s\n", line);
+    while (strcmp(line, "\0") >= STRING_UNEQUAL) {
+        char* str = get_string(&line);
+        if (str != NULL) {
+            printf("str:   %s\n", str);
+        } else {
+            char token = line[0];
+            printf("other: %c\n", token);
+            line += 1;
+        }
+    }
     return;
 }
 
-char* get_string(char* line) {
+char* get_string(char** line) {
     regex_t reegex;
     regmatch_t groups[MAX_REGEX_GROUPS];
     int value;
     value = regcomp(&reegex, "^(\\w+)", REG_EXTENDED);
-    value = regexec(&reegex, line, MAX_REGEX_GROUPS, groups, REGEX_FLAGS);
+    value = regexec(&reegex, *line, MAX_REGEX_GROUPS, groups, REGEX_FLAGS);
 
     char* str = NULL;
 
     if (value == REG_OK) {
-        printf("%s\n", line);
         for (unsigned int g = 0; g < MAX_REGEX_GROUPS; g++){
-            if (groups[g].rm_so == (size_t)-1)
+            if (groups[g].rm_so == (size_t) - 1)
                 break;
-            char sourceCopy[strlen(line) + 1];
-            strcpy(sourceCopy, line);
+            char sourceCopy[strlen(*line) + 1];
+            strcpy(sourceCopy, *line);
             sourceCopy[groups[g].rm_eo] = 0;
             str = sourceCopy + groups[g].rm_so;
         }
+    } else {
+        return NULL;
     }
 
     regfree(&reegex);
+
+    int str_length = strlen(str);
+    *line += str_length;
 
     return str;
 }
