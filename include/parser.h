aaa12ec7c822d0170070fc2e5b985e1646e8ba72
Villy-P <valeriuspetrini@gmail.com>
2024-09-11 15:40:14 -0500

    feat: add newline symbol

--- include/parser.h
+++ include/parser.h
@@ -34,7 +34,8 @@ typedef enum SymbolDef {
     SYMBOL_COMMA,
     SYMBOL_PERIOD,
     SYMBOL_QUESTION,
-    SYMBOL_SLASH
+    SYMBOL_SLASH,
+    SYMBOL_NEWLINE
 } Symbol;
 
 typedef struct TokenDef {
