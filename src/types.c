d6eec114d823d65f1e93c45569f0e878c984a20a
Villy-P <valeriuspetrini@gmail.com>
2024-09-17 15:27:25 -0500

    create type array

--- src/types.c
+++ src/types.c
@@ -1,5 +1,12 @@
+#include <stdlib.h>
+
 #include "types.h"
 
+Identifier** types = NULL;
+unsigned int max_type_length = INITIAL_TYPES_LENGTH;
+unsigned int current_type_length = 0;
+
 void create_base_types() {
-    
+    if (types == NULL)
+        types = malloc(sizeof(Identifier*) * INITIAL_TYPES_LENGTH);
 }
