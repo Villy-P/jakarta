333e4db76a5524794cb8b94c0d7a7f62a4339000
Villy-P <valeriuspetrini@gmail.com>
2024-09-19 15:11:00 -0500

    feat: make Type struct

--- include/types.h
+++ include/types.h
@@ -5,11 +5,19 @@
 
 #define INITIAL_TYPES_LENGTH 64
 
+typedef struct TypeDef {
+    char* name;
+    unsigned char bit_size;
+    size_t lower_bound;
+    size_t upper_bound;
+} Type;
+
 extern Identifier** types;
 extern unsigned int max_type_length;
 extern unsigned int current_type_length;
 
 void create_base_types();
 void add_type(Identifier* type);
+void create_type();
 
 #endif
