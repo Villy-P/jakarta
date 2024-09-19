ee9443595d9d0130ed70c055fc570dfd98ae6839
Villy-P <valeriuspetrini@gmail.com>
2024-09-19 16:08:11 -0500

    feat: get_keyword_from_str

--- src/symbol.c
+++ src/symbol.c
@@ -44,66 +44,35 @@ Symbol get_symbol_from_char(char ch) {
 }
 
 Symbol get_keyword_from_str(char* str) {
-    if (strcmp(str, "as"))
-        return KEYWORD_AS;
-    if (strcmp(str, "assert"))
-        return KEYWORD_ASSERT;
-    if (strcmp(str, "break"))
-        return KEYWORD_BREAK;
-    if (strcmp(str, "case"))
-        return KEYWORD_CASE;
-    if (strcmp(str, "catch"))
-        return KEYWORD_CATCH;
-    if (strcmp(str, "class"))
-        return KEYWORD_CLASS;
-    if (strcmp(str, "constant"))
-        return KEYWORD_CONSTANT;
-    if (strcmp(str, "do"))
-        return KEYWORD_DO;
-    if (strcmp(str, "else"))
-        return KEYWORD_ELSE;
-    if (strcmp(str, "enum"))
-        return KEYWORD_ENUM;
-    if (strcmp(str, "extends"))
-        return KEYWORD_EXTENDS;
-    if (strcmp(str, "finally"))
-        return KEYWORD_FINALLY;
-    if (strcmp(str, "for"))
-        return KEYWORD_FOR;
-    if (strcmp(str, "from"))
-        return KEYWORD_FROM;
-    if (strcmp(str, "func"))
-        return KEYWORD_FUNC;
-    if (strcmp(str, "goto"))
-        return KEYWORD_GOTO;
-    if (strcmp(str, "if"))
-        return KEYWORD_IF;
-    if (strcmp(str, "interface"))
-        return KEYWORD_INTERFACE;
-    if (strcmp(str, "import"))
-        return KEYWORD_IMPORT;
-    if (strcmp(str, "new"))
-        return KEYWORD_NEW;
-    if (strcmp(str, "priv"))
-        return KEYWORD_PRIV;
-    if (strcmp(str, "prot"))
-        return KEYWORD_PROT;
-    if (strcmp(str, "pub"))
-        return KEYWORD_PUB;
-    if (strcmp(str, "ret"))
-        return KEYWORD_RET;
-    if (strcmp(str, "static"))
-        return KEYWORD_STATIC;
-    if (strcmp(str, "switch"))
-        return KEYWORD_SWITCH;
-    if (strcmp(str, "this"))
-        return KEYWORD_THIS;
-    if (strcmp(str, "throws"))
-        return KEYWORD_THROWS;
-    if (strcmp(str, "try"))
-        return KEYWORD_TRY;
-    if (strcmp(str, "typedef"))
-        return KEYWORD_TYPEDEF;
-    if (strcmp(str, "while"))
-        return KEYWORD_WHILE;
+    if (strcmp(str, "as"))        return KEYWORD_AS;
+    if (strcmp(str, "assert"))    return KEYWORD_ASSERT;
+    if (strcmp(str, "break"))     return KEYWORD_BREAK;
+    if (strcmp(str, "case"))      return KEYWORD_CASE;
+    if (strcmp(str, "catch"))     return KEYWORD_CATCH;
+    if (strcmp(str, "class"))     return KEYWORD_CLASS;
+    if (strcmp(str, "constant"))  return KEYWORD_CONSTANT;
+    if (strcmp(str, "do"))        return KEYWORD_DO;
+    if (strcmp(str, "else"))      return KEYWORD_ELSE;
+    if (strcmp(str, "enum"))      return KEYWORD_ENUM;
+    if (strcmp(str, "extends"))   return KEYWORD_EXTENDS;
+    if (strcmp(str, "finally"))   return KEYWORD_FINALLY;
+    if (strcmp(str, "for"))       return KEYWORD_FOR;
+    if (strcmp(str, "from"))      return KEYWORD_FROM;
+    if (strcmp(str, "func"))      return KEYWORD_FUNC;
+    if (strcmp(str, "goto"))      return KEYWORD_GOTO;
+    if (strcmp(str, "if"))        return KEYWORD_IF;
+    if (strcmp(str, "interface")) return KEYWORD_INTERFACE;
+    if (strcmp(str, "import"))    return KEYWORD_IMPORT;
+    if (strcmp(str, "new"))       return KEYWORD_NEW;
+    if (strcmp(str, "priv"))      return KEYWORD_PRIV;
+    if (strcmp(str, "prot"))      return KEYWORD_PROT;
+    if (strcmp(str, "pub"))       return KEYWORD_PUB;
+    if (strcmp(str, "ret"))       return KEYWORD_RET;
+    if (strcmp(str, "static"))    return KEYWORD_STATIC;
+    if (strcmp(str, "switch"))    return KEYWORD_SWITCH;
+    if (strcmp(str, "this"))      return KEYWORD_THIS;
+    if (strcmp(str, "throws"))    return KEYWORD_THROWS;
+    if (strcmp(str, "try"))       return KEYWORD_TRY;
+    if (strcmp(str, "typedef"))   return KEYWORD_TYPEDEF;
+    if (strcmp(str, "while"))     return KEYWORD_WHILE;
 }
