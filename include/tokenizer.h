4f09b63cbb7f22a244daa3f8d2e5cf72a0602cae
Villy-P <valeriuspetrini@gmail.com>
2024-09-23 15:46:06 -0500

    feat: add Tokenzier struct

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -1,4 +1,12 @@
 #ifndef __JAKARTA_TOKENIZER_H__
 #define __JAKARTA_TOKENIZER_H__
 
+#include "token.h"
+
+typedef struct TokenzierDef {
+    Token** tokens;
+    unsigned int max_token_length;
+    unsigned int current_token_length;
+} Tokenizer;
+
 #endif
