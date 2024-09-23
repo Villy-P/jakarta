36575636cb602bcafebd14b964fd2f01afdaf083
Villy-P <valeriuspetrini@gmail.com>
2024-09-23 16:02:21 -0500

    feat: remove global token variables

--- src/token.c
+++ src/token.c
@@ -4,10 +4,6 @@
 
 #include "token.h"
 
-Token** tokens = NULL;
-unsigned int max_token_length = INITIAL_TOKENS_LENGTH;
-unsigned int current_token_length = 0;
-
 Token* create_token(Symbol symbol, unsigned int line, unsigned int col, char* content) {
     Token* item = malloc(sizeof(Token));
     item->symbol = symbol;
