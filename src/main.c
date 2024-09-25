e7589005ede089f4c933aca5d5d0d0cbbdacc82c
Villy-P <valeriuspetrini@gmail.com>
2024-09-25 15:37:10 -0500

    feat: add macros to main

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 
 #define STRING_EQUAL 0
 #define FILE_NAME_SIZE 256
+#define INPUT_FILE_ARGUMENT "-f"
+#define OUTPUT_FILE_ARGUMENT "-o"
 
 int main(int argc, char *argv[]) {
     create_base_types();
@@ -16,13 +18,13 @@ int main(int argc, char *argv[]) {
     char input_file[FILE_NAME_SIZE];
     char output_file[FILE_NAME_SIZE];
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-f") == STRING_EQUAL) {
+        if (strcmp(argv[i], INPUT_FILE_ARGUMENT) == STRING_EQUAL) {
             if (i == argc - 1)
-                jakarta_error_no_file_location("-f");
+                jakarta_error_no_file_location(INPUT_FILE_ARGUMENT);
             strncpy(input_file, argv[i + 1], FILE_NAME_SIZE);
-        } else if (strcmp(argv[i], "-o") == STRING_EQUAL) {
+        } else if (strcmp(argv[i], OUTPUT_FILE_ARGUMENT) == STRING_EQUAL) {
             if (i == argc - 1)
-                jakarta_error_no_file_location("-o");
+                jakarta_error_no_file_location(OUTPUT_FILE_ARGUMENT);
             strncpy(output_file, argv[i + 1], FILE_NAME_SIZE);
         }
     }
