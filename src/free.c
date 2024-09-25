647cc8be77455a60bd642103c86fcc794460c611
Villy-P <valeriuspetrini@gmail.com>
2024-09-25 15:14:04 -0500

    feat: create free_token

--- src/free.c
+++ src/free.c
@@ -10,9 +10,12 @@ void free_type_data(void) {
 }
 
 void free_tokenizer(Tokenizer* tokenizer) {
-    for (unsigned short i = 0; i < tokenizer->current_token_length; i++) {
-        free(tokenizer->tokens[i]->content);
-        free(tokenizer->tokens[i]);
-    }
+    for (unsigned short i = 0; i < tokenizer->current_token_length; i++)
+        free_token(tokenizer->tokens[i]);
     free(tokenizer);
+}
+
+void free_token(Token* token) {
+    free(token->content);
+    free(token]);
 }
