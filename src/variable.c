9b4dd02976304b70acb15996ed710f8e7720f912
Villy-P <valeriuspetrini@gmail.com>
2024-10-23 15:52:55 -0500

    feat: add create variable function

--- src/variable.c
+++ src/variable.c
@@ -1,1 +1,11 @@
 #include "variable.h"
+
+Variable* create_variable(Type* type, char* name, unsigned short scope) {
+    Variable* variable = malloc(sizeof(Variable));
+    variable->type = malloc(sizeof(Type));
+    variable->name = malloc(strlen(name) + 1);
+    variable->type = type;
+    strcpy(variable->name, name);
+    variable->scope = scope;
+    return variable;
+}
