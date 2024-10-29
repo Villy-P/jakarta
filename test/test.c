e2d5c1432831ef6929c6eed028b1449fb39c1c51
Villy-P <valeriuspetrini@gmail.com>
2024-10-29 15:30:11 -0500

    refactor: Add number symbol tests in test.c

--- test/test.c
+++ test/test.c
@@ -1,6 +1,17 @@
+#include "symbol.h"
+
 #include <assert.h>
 
 int main() {
-    assert(1 == 1);
+    assert(is_number_symbol("10"));
+    assert(is_number_symbol("100"));
+    assert(is_number_symbol("10.10"));
+    assert(is_number_symbol("10.120"));
+    assert(is_number_symbol("010"));
+    assert(is_number_symbol("0b10100"));
+    assert(is_number_symbol("0xAF3"));
+    assert(is_number_symbol("07"));
+    assert(is_number_symbol("10e2"));
+    assert(is_number_symbol("10e3432"));
     return 0;
 }
