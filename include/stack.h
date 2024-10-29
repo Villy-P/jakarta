8c167de6965f08dc0b2249684f73b2aca2d2323c
Villy-P <valeriuspetrini@gmail.com>
2024-10-28 22:04:35 -0500

    feat: add add_to_stack function
    Add the add_to_stack function to the stack implementation. This function allows adding a new element to the top of the stack. It allocates memory for a new node, assigns the value to the node's content, updates the node's next pointer to point to the current head of the stack, and updates the stack's head and size accordingly.

--- include/stack.h
+++ include/stack.h
@@ -14,5 +14,6 @@ typedef struct StackNodeDef {
 } StackNode;
 
 Stack* create_stack();
+void add_to_stack(Stack* stack, STACK_TYPE value);
 
 #endif
--- src/stack.c
+++ src/stack.c
@@ -5,3 +5,11 @@ Stack* create_stack() {
     stack->size = 0;
     return stack;
 }
+
+void add_to_stack(Stack* stack, STACK_TYPE value) {
+    StackNode* new_node = (StackNode*)malloc(sizeof(StackNode));
+    new_node->content = value;
+    new_node->next = stack->head;
+    stack->head = new_node;
+    stack->size++;
+}
