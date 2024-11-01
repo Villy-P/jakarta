13514c9c549d72c462c43a2a5966418129a9b742
Villy-P <valeriuspetrini@gmail.com>
2024-10-31 19:25:48 -0500

    feat: Initialize nodes array with a default capacity in ASTNode creation

--- src/ast.c
+++ src/ast.c
@@ -6,8 +6,8 @@ ASTNode* createASTNode(ASTIdentifier identifier, Token* token) {
     ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode));
     node->identifier = identifier;
     node->token = token;
-    node->nodes = NULL;
     node->nodes_length = 0;
-    node->nodes_capacity = 0;
+    node->nodes_capacity = 2;
+    node->nodes = (ASTNode**)malloc(node->nodes_capacity * sizeof(ASTNode*));
     return node;
 }
