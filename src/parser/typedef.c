06519d6448e58b40c855e9171425ef1d213450da
Villy-P <valeriuspetrini@gmail.com>
2024-11-01 16:19:16 -0500

    feat: Update create_type_alias to use type_alias content for better clarity

--- src/parser/typedef.c
+++ src/parser/typedef.c
@@ -10,7 +10,7 @@ void parse_typedef(Tokenizer* tokenizer, ASTNode* ast_node) {
     Type* type = get_type(tokenizer, type_name->content);
     if (type == NULL)
         jakarta_error_undefined_identifier(type_name->content);
-    TypeAlias* alias = create_type_alias(type_alias, type);
+    TypeAlias* alias = create_type_alias(type_alias->content, type);
     add_type_alias(tokenizer, alias);
     Token* semicolon = peek_consume(tokenizer, SYMBOL_SEMICOLON);
 
