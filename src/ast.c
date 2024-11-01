a775c0f2117a22f6b16acbd1138b036592426475
Villy-P <valeriuspetrini@gmail.com>
2024-10-31 19:27:08 -0500

    feat: Add function to dynamically add child nodes to ASTNode

--- src/ast.c
+++ src/ast.c
@@ -11,3 +11,11 @@ ASTNode* createASTNode(ASTIdentifier identifier, Token* token) {
     node->nodes = (ASTNode**)malloc(node->nodes_capacity * sizeof(ASTNode*));
     return node;
 }
+
+void addASTNode(ASTNode* parent, ASTNode* child) {
+    if (parent->nodes_length >= parent->nodes_capacity) {
+        parent->nodes_capacity *= 2;
+        parent->nodes = (ASTNode**)realloc(parent->nodes, parent->nodes_capacity * sizeof(ASTNode*));
+    }
+    parent->nodes[parent->nodes_length++] = child;
+}
