b6e9b1503e01ade83c8e1e79b8c319e696b1ce60
Villy-P <valeriuspetrini@gmail.com>
2024-11-01 16:10:40 -0500

    feat: Add AST nodes for typedef parsing in parse_typedef function

--- src/parser/typedef.c
+++ src/parser/typedef.c
@@ -13,4 +13,8 @@ void parse_typedef(Tokenizer* tokenizer, ASTNode* ast_node) {
     TypeAlias* alias = create_type_alias(type_alias, type);
     add_type_alias(tokenizer, alias);
     peek_consume(tokenizer, SYMBOL_SEMICOLON);
+
+    ASTNode* typedef_node  = create_ast_node(AST_IDENTIFIER_TYPE_DEFINITION, NULL);
+    ASTNode* typedef_alias = create_ast_node(AST_IDENTIFIER_TYPE_DEFINITION_ALIAS, type_alias);
+    ASTNode* typedef_type  = create_ast_node(AST_IDENTIFIER_TYPE_DEFINITION_TYPE, type_name);
 }
