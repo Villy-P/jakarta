33553e073f1440e7c34ad0b6ae6e36461efd2a63
Villy-P <valeriuspetrini@gmail.com>
2024-11-09 14:52:52 -0600

    feat: Add function parsing support in the parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,6 +6,8 @@ void parse(Tokenizer* tokenizer, ASTNode* ast_node) {
     while (tokenizer->current_token_length > 0) {
         if (peek(tokenizer, KEYWORD_TYPEDEF))
             parse_typedef(tokenizer, ast_node);
+        else if (peek(tokenizer, KEYWORD_FUNC))
+            parse_func(tokenizer, ast_node);
         else
             consume(tokenizer);
     }
