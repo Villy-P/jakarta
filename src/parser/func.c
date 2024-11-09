781dfe6c4e756a19afc0af845c753c94d5f49181
Villy-P <valeriuspetrini@gmail.com>
2024-11-09 15:00:51 -0600

    feat: Enhance parse_func to create AST nodes for function definition and parameters

--- src/parser/func.c
+++ src/parser/func.c
@@ -9,6 +9,14 @@ void parse_func(Tokenizer* tokenizer, ASTNode* ast_node) {
     Token* func_type = peek_consume(tokenizer, SYMBOL_STRING);
     Token* func_name = peek_consume(tokenizer, SYMBOL_STRING);
     Token* open_parenthesis = peek_consume(tokenizer, SYMBOL_OPEN_PARENTHESIS);
+
+    ASTNode* func_node = create_ast_node(AST_IDENTIFIER_FUNCTION_DEFINITION, func_name);
+    ASTNode* func_type_node = create_ast_node(AST_IDENTIFIER_FUNCTION_RETURN_TYPE, func_type);
+    ASTNode* func_params_node = create_ast_node(AST_IDENTIFIER_FUNCTION_PARAMETERS, NULL);
+
+    add_ast_node(func_node, func_type_node);
+    add_ast_node(func_node, func_params_node);
+
     while (!peek(tokenizer, SYMBOL_CLOSE_PARENTHESIS)) {
         Token* arg_type = peek_consume(tokenizer, SYMBOL_STRING);
         Token* arg_name = peek_consume(tokenizer, SYMBOL_STRING);
@@ -16,6 +24,11 @@ void parse_func(Tokenizer* tokenizer, ASTNode* ast_node) {
         Type* argtype = get_type(tokenizer, arg_type->content);
         if (argtype == NULL)
             jakarta_error_undefined_identifier(arg_type);
+
+        
+
+        free_token(comma);
+        free_type(argtype);
     }
     Token* close_parenthesis = consume(tokenizer);
     Token* open_brace = peek_consume(tokenizer, SYMBOL_OPEN_BRACE);
@@ -27,4 +40,5 @@ void parse_func(Tokenizer* tokenizer, ASTNode* ast_node) {
     free_token(open_parenthesis);
     free_token(close_parenthesis);
     free_token(open_brace);
+    free_type(type);
 }
