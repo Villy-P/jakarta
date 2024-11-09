e63ecc2af9dee5a1b56af5df334bf1862a0ce183
Villy-P <valeriuspetrini@gmail.com>
2024-11-09 14:54:49 -0600

    feat: Enhance parse_func to handle function type and name validation

--- src/parser/func.c
+++ src/parser/func.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 
 #include "parser.h"
+#include "error.h"
 
 void parse_func(Tokenizer* tokenizer, ASTNode* ast_node) {
-    
+    Token* func_keyword = consume(tokenizer);
+    Token* func_type = peek_consume(tokenizer, SYMBOL_STRING);
+    Token* func_name = peek_consume(tokenizer, SYMBOL_STRING);
+    Type* type = get_type(tokenizer, func_type->content);
+    if (type == NULL)
+        jakarta_error_undefined_identifier(func_type);
 }
