1eb25a69d780f63d8a2af2dae4de1de9ee6be41f
Villy-P <valeriuspetrini@gmail.com>
2024-11-09 15:02:09 -0600

    feat: Add support for function parameters and their types in AST

--- src/parser/func.c
+++ src/parser/func.c
@@ -25,7 +25,11 @@ void parse_func(Tokenizer* tokenizer, ASTNode* ast_node) {
         if (argtype == NULL)
             jakarta_error_undefined_identifier(arg_type);
 
-        
+        ASTNode* arg_node = create_ast_node(AST_IDENTIFIER_FUNCTION_PARAMETER, arg_name);
+        ASTNode* arg_type_node = create_ast_node(AST_IDENTIFIER_FUNCTION_PARAMETER_TYPE, arg_type);
+
+        add_ast_node(arg_node, arg_type_node);
+        add_ast_node(func_params_node, arg_node);
 
         free_token(comma);
         free_type(argtype);
@@ -35,6 +39,8 @@ void parse_func(Tokenizer* tokenizer, ASTNode* ast_node) {
     Type* type = get_type(tokenizer, func_type->content);
     if (type == NULL)
         jakarta_error_undefined_identifier(func_type);
+
+    add_ast_node(ast_node, func_node);
 
     free_token(func_keyword);
     free_token(open_parenthesis);
