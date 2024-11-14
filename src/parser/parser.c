645f516e1240f3a6892523b9ea20e263fcae8e27
Villy-P <valeriuspetrini@gmail.com>
2024-11-13 19:12:11 -0600

    feat: Remove debug print statement from parse function

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,7 +2,6 @@
 #include "debug.h"
 
 void parse(Tokenizer* tokenizer, ASTNode* ast_node) {
-    printf("Parsing Token: %s\n", tokenizer->tokens[0]->content);
     if (peek(tokenizer, KEYWORD_TYPEDEF))
         parse_typedef(tokenizer, ast_node);
     else if (peek(tokenizer, KEYWORD_FUNC))
