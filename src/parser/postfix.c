d0ea09cca742a0bdd7da3abb6140d518feaaffac
Villy-P <valeriuspetrini@gmail.com>
2024-12-04 16:03:45 -0600

    feat: Include tokenizer.h in postfix.h and update precedence function parameter name

--- src/parser/postfix.c
+++ src/parser/postfix.c
@@ -2,11 +2,11 @@
 
 #include "postfix.h"
 
-unsigned int precedence(char* operator) {
-    if (operator == NULL)
+unsigned int precedence(char* op) {
+    if (op == NULL)
         return 0;
-    if (strcmp(operator, "+") == 0 || strcmp(operator, "-") == 0)
+    if (strcmp(op, "+") == 0 || strcmp(op, "-") == 0)
         return 1;
-    if (strcmp(operator, "*") == 0 || strcmp(operator, "/") == 0)
+    if (strcmp(op, "*") == 0 || strcmp(op, "/") == 0)
         return 2;
 }
