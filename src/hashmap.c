217289842b29ae1c207cea3004e710f0a374dc78
Villy-P <valeriuspetrini@gmail.com>
2025-01-14 15:34:54 -0600

    feat: Implement insert function for HashMap and include stddef.h

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,5 +1,7 @@
 #include "hashmap.h"
 
+#include <stddef.h>
+
 int hash(char* key) {
     const int p = 31;
     const int m = 1e9 + 9;
@@ -10,4 +12,22 @@ int hash(char* key) {
         p_pow = (p_pow * p) % m;
     }
     return hash_value % HASHMAP_ARRAY_SIZE;
+}
+
+int insert(HashMap* hashmap, char* key, void* value) {
+    int index = hash(key);
+    HashNode* node = hashmap->array[index];
+    while (node != NULL) {
+        if (strcmp(node->key, key) == 0) {
+            node->value = value;
+            return 0;
+        }
+        node = node->next;
+    }
+    HashNode* new_node = (HashNode*)malloc(sizeof(HashNode));
+    new_node->key = key;
+    new_node->value = value;
+    new_node->next = hashmap->array[index];
+    hashmap->array[index] = new_node;
+    return 0;
 }
