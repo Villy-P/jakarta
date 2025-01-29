a2907338b0b605cbaa5f38b7c1b2c51fd7ebd2fd
Villy-P <valeriuspetrini@gmail.com>
2025-01-29 16:21:02 -0600

    feat: Improve memory safety in function and parameter creation with strncpy_s

--- src/types/function.c
+++ src/types/function.c
@@ -1,11 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "types/function.h"
 
 FunctionDefinition* create_function_definition(const char* name, const char* return_type) {
     FunctionDefinition* function_definition = malloc(sizeof(FunctionDefinition));
     function_definition->name = malloc(strlen(name) + 1);
     function_definition->return_type = malloc(strlen(return_type) + 1);
-    strcpy(function_definition->name, name);
-    strcpy(function_definition->return_type, return_type);
+    strncpy_s(function_definition->name, strlen(name) + 1, name, strlen(name) + 1);
+    strncpy_s(function_definition->return_type, strlen(return_type) + 1, return_type, strlen(return_type) + 1);
     return function_definition;
 }
 
@@ -13,7 +16,7 @@ Parameter* create_parameter(const char* name, const char* type) {
     Parameter* parameter = malloc(sizeof(Parameter));
     parameter->name = malloc(strlen(name) + 1);
     parameter->type = malloc(strlen(type) + 1);
-    strcpy(parameter->name, name);
-    strcpy(parameter->type, type);
+    strncpy_s(parameter->name, strlen(name) + 1, name, strlen(name) + 1);
+    strncpy_s(parameter->type, strlen(type) + 1, type, strlen(type) + 1);
     return parameter;
 }
