477ea5a33d9c19aec21d8007ef7ad1fd2253aed2
Villy-P <valeriuspetrini@gmail.com>
2025-01-29 16:11:21 -0600

    feat: Refactor function management to support parameter creation and update includes

--- include/types/function.h
+++ include/types/function.h
@@ -1,15 +1,20 @@
-#ifndef __JAKARTA_TYPES_H__
-#define __JAKARTA_TYPES_H__
+#ifndef __JAKARTA_FUNCTION_H__
+#define __JAKARTA_FUNCTION_H__
+
+typedef struct ParameterDef {
+    char* name;
+    char* type;
+} Parameter;
 
 typedef struct FunctionDefinitionDef {
     char* name;
     char* return_type;
-    char** parameters;
+    Parameter** parameters;
     unsigned short parameter_count;
     unsigned short current_parameter;
 } FunctionDefinition;
 
-FunctionDefinition* create_function_definition(const char* name, const char* return_type, unsigned short parameter_count);
-void add_parameter(FunctionDefinition* function_definition, const char* parameter);
+FunctionDefinition* create_function_definition(const char* name, const char* return_type);
+Parameter* create_parameter(const char* name, const char* type);
 
 #endif
--- src/types/function.c
+++ src/types/function.c
@@ -1,19 +1,19 @@
-#include "function.h"
+#include "types/function.h"
 
-FunctionDefinition* create_function_definition(const char* name, const char* return_type, unsigned short parameter_count) {
+FunctionDefinition* create_function_definition(const char* name, const char* return_type) {
     FunctionDefinition* function_definition = malloc(sizeof(FunctionDefinition));
     function_definition->name = malloc(strlen(name) + 1);
     function_definition->return_type = malloc(strlen(return_type) + 1);
     strcpy(function_definition->name, name);
     strcpy(function_definition->return_type, return_type);
-    function_definition->parameters = malloc(sizeof(char*) * parameter_count);
-    function_definition->parameter_count = parameter_count;
-    function_definition->current_parameter = 0;
     return function_definition;
 }
 
-void add_parameter(FunctionDefinition* function_definition, const char* parameter) {
-    function_definition->parameters[function_definition->current_parameter] = malloc(strlen(parameter) + 1);
-    strcpy(function_definition->parameters[function_definition->current_parameter], parameter);
-    function_definition->current_parameter++;
+Parameter* create_parameter(const char* name, const char* type) {
+    Parameter* parameter = malloc(sizeof(Parameter));
+    parameter->name = malloc(strlen(name) + 1);
+    parameter->type = malloc(strlen(type) + 1);
+    strcpy(parameter->name, name);
+    strcpy(parameter->type, type);
+    return parameter;
 }
