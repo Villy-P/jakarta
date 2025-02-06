68c788ee8896427e05286cbf30fdeac214bc4369
Villy-P <valeriuspetrini@gmail.com>
2025-02-06 15:34:03 -0600

    feat: Implement return statement handling in parser with AST integration

--- src/parser/ret.c
+++ src/parser/ret.c
@@ -1,5 +1,14 @@
 #include "parser.h"
+#include "stack.h"
 
 void parse_ret(Tokenizer* tokenizer, ASTNode* ast_node) {
-    
+    Token* ret = consume(tokenizer);
+
+    Stack* postfix = infix_to_postfix(tokenizer);
+    ASTNode* expression = postfix_to_ast(postfix);
+
+    add_ast_node(ast_node, expression);
+
+    ASTNode* ret_node = create_ast_node(AST_IDENTIFIER_RETURN, ret);
+    add_ast_node(ast_node, ret_node);
 }
