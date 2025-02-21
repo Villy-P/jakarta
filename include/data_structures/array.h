7d411c1935d52c4e9c062465b4864ae604c3172c
Villy-P <valeriuspetrini@gmail.com>
2025-02-21 15:36:28 -0600

    feat: Implement dynamic array data structure with basic operations

---
+++ include/data_structures/array.h
@@ -0,0 +1,16 @@
+#ifndef __JAKARTA_ARRAY_H__
+#define __JAKARTA_ARRAY_H__
+
+typedef struct ArrayDef {
+    void** data;
+    unsigned int length;
+    unsigned int capacity;
+} Array;
+
+Array* create_array(unsigned int initial_size);
+void add_to_array(Array* array, void* data);
+void get_from_array(Array* array, unsigned int index, void* target);
+void remove_from_array(Array* array, unsigned int index);
+void free_array(Array* array);
+
+#endif
