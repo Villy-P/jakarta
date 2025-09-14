93e7fc6f88da288000727b2e8c6432519e0e115f
Villy-P <valeriuspetrini@gmail.com>
2025-09-13 20:50:35 -0500

    refactor: remove parse_variable and parse_variable_declaration functions, update parser and syntax headers

--- include/parser.h
+++ include/parser.h
@@ -7,5 +7,3 @@
 void parse_for(Tokenizer* tokenizer, ASTNode* ast_node);
 void parse_if(Tokenizer* tokenizer, ASTNode* ast_node);
 void parse_ret(Tokenizer* tokenizer, ASTNode* ast_node);
-void parse_variable(Tokenizer* tokenizer, ASTNode* ast_node);
-ASTNode* parse_variable_declaration(Tokenizer* tokenizer, FunctionDefinition* function_definition, ASTNode* class);
--- include/syntax.h
+++ include/syntax.h
@@ -34,3 +34,5 @@ void parse_class(Tokenizer* tokenizer, ASTNode* ast_node);
 void parse_func(Tokenizer* tokenizer, ASTNode* ast_node);
 void parse_func_call(Tokenizer* tokenizer, ASTNode* ast_node, FunctionDefinition* function);
 void parse_typedef(Tokenizer* tokenizer, ASTNode* ast_node);
+void parse_variable(Tokenizer* tokenizer, ASTNode* ast_node);
+ASTNode* parse_variable_declaration(Tokenizer* tokenizer, FunctionDefinition* function_definition, ASTNode* class);
