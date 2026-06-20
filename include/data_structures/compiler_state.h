6609d1b7400fb2907c3fba3facd547ad5fa047bf
Villy-P <valeriuspetrini@gmail.com>
2026-06-19 22:55:19 -0500

    refactor: add type and function registries to CompilerState and implement corresponding entry creation functions

--- include/data_structures/compiler_state.h
+++ include/data_structures/compiler_state.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "data_structures/hashmap.h"
 #include "syntax.h"
 #include "data_structures/array.h"
+#include "types.h"
 
 // forward declarations
 typedef struct ASTNodeDef ASTNode;
@@ -12,6 +14,20 @@ typedef struct {
     ASTNode *root;
 } ForestEntry;
 
+typedef struct {
+    char* name;
+    char* return_type;
+    int parameter_count;
+    Array* parameter_types;
+    ASTNode* body;
+} FunctionRegistryEntry;
+
+typedef struct {
+    char* name;
+    unsigned char bit_size;
+    TypeOptions option;
+} TypeRegistryEntry;
+
 typedef struct CompilerStateDef {
     Array* forest;
     Array* files_to_parse;
@@ -20,8 +36,12 @@ typedef struct CompilerStateDef {
     int error_count;
 
     SymbolTable* symbol_tree;
+    HashMap* type_registry;
+    HashMap* function_registry;
 } CompilerState;
 
+FunctionRegistryEntry* create_function_registry_entry(const char* name, const char* return_type, int parameter_count, Array* parameter_types, ASTNode* body);
+TypeRegistryEntry* create_type_registry_entry(const char* name, unsigned char bit_size, TypeOptions option);
 ForestEntry* create_forest_entry(char* file_path, ASTNode* root);
 CompilerState* create_compiler_state();
 
--- src/data_structures/compiler_state.c
+++ src/data_structures/compiler_state.c
@@ -13,6 +13,8 @@ CompilerState* create_compiler_state() {
     state->error_list = create_array(20);
     state->error_count = 0;
     state->symbol_tree = create_symbol_table();
+    state->type_registry = create_hashmap();
+    state->function_registry = create_hashmap();
     return state;
 }
 
@@ -34,4 +36,35 @@ void print_error_list(CompilerState* state) {
         char* error_message = (char*)get_from_array(state->error_list, i);
         printf("%s\n", error_message);
     }
+}
+
+TypeRegistryEntry* create_type_registry_entry(const char* name, unsigned char bit_size, TypeOptions option) {
+    TypeRegistryEntry* entry = malloc(sizeof(TypeRegistryEntry));
+    if (!entry)
+        jakarta_error(ERR_MALLOC_FAIL, NULL, "TypeRegistryEntry");
+    entry->name = malloc(strlen(name) + 1);
+    if (!entry->name)
+        jakarta_error(ERR_MALLOC_FAIL, NULL, "TypeRegistryEntry name");
+    strcpy(entry->name, name);
+    entry->bit_size = bit_size;
+    entry->option = option;
+    return entry;
+}
+
+FunctionRegistryEntry* create_function_registry_entry(const char* name, const char* return_type, int parameter_count, Array* parameter_types, ASTNode* body) {
+    FunctionRegistryEntry* entry = malloc(sizeof(FunctionRegistryEntry));
+    if (!entry)
+        jakarta_error(ERR_MALLOC_FAIL, NULL, "FunctionRegistryEntry");
+    entry->name = malloc(strlen(name) + 1);
+    if (!entry->name)
+        jakarta_error(ERR_MALLOC_FAIL, NULL, "FunctionRegistryEntry name");
+    strcpy(entry->name, name);
+    entry->return_type = malloc(strlen(return_type) + 1);
+    if (!entry->return_type)
+        jakarta_error(ERR_MALLOC_FAIL, NULL, "FunctionRegistryEntry return type");
+    strcpy(entry->return_type, return_type);
+    entry->parameter_count = parameter_count;
+    entry->parameter_types = parameter_types;
+    entry->body = body;
+    return entry;
 }
