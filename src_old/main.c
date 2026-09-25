90521a7ca2e5777f12cb688a2d37cc61acab98af
Valerius Petrini <valeriuspetrini@gmail.com>
2026-09-25 12:42:58 -0500

    feat(migration): migrate to new file directory

---
+++ src_old/main.c
@@ -0,0 +1,36 @@
+#ifdef __WIN32
+#include <errhandlingapi.h>
+#endif
+
+#include "cmd.h"
+#include "core.h"
+#include "data_structures/compiler_state.h"
+#include "data_structures/tokenizer.h"
+#include "debug.h"
+
+int main(int argc, char* argv[]) {
+    setup_logs();
+    log_msg(logs.main, "[PROGRAM] Program started with %d arguments", argc);
+
+#ifdef __WIN32
+    SetUnhandledExceptionFilter(handle_seg_fault);
+#else
+    install_seg_fault_handler();
+#endif
+
+    log_msg(logs.main, "[PROGRAM] Set up segmentation fault handler");
+
+    CmdArgs args = {0};
+    parse_args(argc, argv, &args);
+
+    CompilerState* state = create_compiler_state();
+
+    create_base_types(state);
+    add_built_in_functions(state);
+
+    jakarta_cmd_read_file(args.input_file, state);
+    jakarta_cmd_out_file(args.output_file);
+
+    log_msg(logs.main, "[PROGRAM] Program finished");
+    cleanup_logs();
+}
